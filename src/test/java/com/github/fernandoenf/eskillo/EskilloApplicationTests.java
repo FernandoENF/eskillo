db77066e5a052f7477bbe4127131e990a3a66a27
FernandoENF <fernandoengh@gmail.com>
2020-09-04 19:03:27 -0300

    first commit

---
+++ src/test/java/com/github/fernandoenf/eskillo/EskilloApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.fernandoenf.eskillo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EskilloApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
