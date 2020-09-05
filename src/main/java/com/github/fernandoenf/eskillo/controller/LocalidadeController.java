8c17c4c38290e2daaf4dbe5f19efbecd59e51934
FernandoENF <fernandoengh@gmail.com>
2020-09-04 22:36:22 -0300

    Update v0.8 - Implementado os métodos HTTP para a classe Localidade

--- src/main/java/com/github/fernandoenf/eskillo/controller/LocalidadeController.java
+++ src/main/java/com/github/fernandoenf/eskillo/controller/LocalidadeController.java
@@ -2,14 +2,31 @@ package com.github.fernandoenf.eskillo.controller;
 
 import com.github.fernandoenf.eskillo.model.Localidade;
 
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocalidadeController {
     
-    @GetMapping()
-    public Localidade obterLocalidade() {
-        return new Localidade(id, nome, uf);
+    @GetMapping("/localidades")
+    public String obterLocalidade() {
+        return "GET LOCALIDADE FUNCIONANDO!";
+    }
+
+    @PostMapping("/localidades")
+    public String postLocalidade(){
+        return "POST LOCALIDADE FUNCIONANDO!";
+    }
+    @PutMapping("/localidades")
+    public String putLocalidade(){
+        return "PUT LOCALIDADE FUNCIONANDO!";
+
+    }
+    @DeleteMapping("/localidades")
+    public String deleteLocalidade(){
+        return "DELETE LOCALIDADE FUNCIONANDO!";
     }
 }
